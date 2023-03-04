311f0e14c653054c8d4c408b17ec1ca932ffe48c
Asraf432 <126665494+Asraf432@users.noreply.github.com>
2023-03-04 20:15:23 +0600

    Update Test.py

--- Test.py
+++ Test.py
@@ -414,9 +414,7 @@ class Main:
 		print("\033[1;32m [1] First You join group")
 		print("\033[1;33m [2] Exit")
 		print("")
-		ASRAF AHMED = input("\n\033[1;36m  Chose ==> \033[1;32m")
-		if ASRAF AHMED in ["", " "]:
-			exit()
+	
 		elif ASRAF AHMED in ["2", "02"]:
 			print("    Thanks🥰♥️")
 			exit()
